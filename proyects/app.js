e8a9943e986ffb1484a8f159f6f623473da3e6aa
JoelMtr <yoelonteromoreta>
2023-10-10 21:04:41 -0400

    First Commit

--- proyects/app.js
+++ proyects/app.js
@@ -4,27 +4,42 @@ let MY_FORM = document.getElementById("myForm");
 let ul = document.getElementById("ul");
 let li = document.createElement("li");
 const contador = 10;
-// let padre = document.getElementById('padre');
+let padre = document.getElementById("padre");
+let mostrar = padre;
+    
 
 function addElement() {
   
   myButton.addEventListener("click", function () {
-    let padre = document.getElementById("padre");
-    let mostrar = padre;
-    mostrar.innerHTML += `<span class="mySpan">${myInput.value}</span>`;
-    myInput.value = "";
+    
+    if(myInput.value.trim() === ''){
+      alert('Por Favor Ingrese Un Valor Valido!!');
+      return;
+    }
+    else{
 
+    }
+
+    mostrar.innerHTML += `<span id="mySpan" class="mySpan">${myInput.value}</span>`;
+    myInput.value = "";
+    
     if (mostrar.childElementCount >= contador) {
-      alert(`Limite de Tareas Superadas ${contador}`);
+      // alert(`Limite de Tareas Superadas ${contador}`);
+      myInput.remove();
+      myButton.remove();
+      let error = '<h1 class="error">Maximo de tareas superadas</h1>';
+      MY_FORM.innerHTML = error;
     }
   });
 }
 
+
+
 myButton.addEventListener("click", function (event) {
   event.preventDefault();
   console.log(myInput.value);
-  myInput.focus();
+  
 });
 
-// valorVacio();
+
 addElement();
