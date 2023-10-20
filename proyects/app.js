be52e2e67d50f268dd3b12d0f5117f2f6158e70a
JoelMtr <yoelonteromoreta>
2023-10-19 22:07:49 -0400

    Save

--- proyects/app.js
+++ proyects/app.js
@@ -1,47 +1,52 @@
 let myInput = document.getElementById("enter");
 let myButton = document.getElementById("buttonPadre");
-let MY_FORM = document.getElementById("myForm");
-let ul = document.getElementById("ul");
-let li = document.createElement("li");
-const contador = 10;
+const MY_FORM = document.getElementById("myForm");
+const CONTADOR = 10;
+let id = 0;
 let padre = document.getElementById("padre");
 let mostrar = padre;
 let mySpan = document.getElementById('mySpan');
-    
+let removeButton = document.getElementById('removeButton');
+let createButton = document.createElement('button');
+
+
+
 
 function addElement() {
   
   myButton.addEventListener("click", function () {
     
-    if(myInput.value.trim() === ''){
-      alert('Por Favor Ingrese Un Valor Valido!!');
-      return;
+    if(myInput.value.trim() === ''){ 
+      window.location.href = 'error.html'
+    
     }
-    else{
-
+    else {
+      id++;
+      mostrar.innerHTML += `
+      <div id="btn${id} btnAll"
+        <span class="mySpan">${myInput.value}</span>
+        <button id="removeButton" class="removeButton">Eliminar</button>
+      </div>`;
     }
-
-    mostrar.innerHTML += `<span id="mySpan" class="mySpan">${myInput.value}</span>`;
+    // mostrar.innerHTML += `<span onclick="removeButton()" id="mySpan" class="mySpan">${myInput.value}</span>`;
     myInput.value = "";
     myInput.focus();
     
-    if (mostrar.childElementCount >= contador) {
-      // alert(`Limite de Tareas Superadas ${contador}`);
+    if (mostrar.childElementCount >= CONTADOR) {
       myInput.remove();
       myButton.remove();
-      let error = '<h1 class="error">Maximo de tareas superadas</h1>';
+      let error = `<h1 class="error">Maximo de tareas superadas ${CONTADOR}</h1>`;
       MY_FORM.innerHTML = error;
     }
   });
 }
 
-
-
 myButton.addEventListener("click", function (event) {
   event.preventDefault();
   console.log(myInput.value);
+  console.log(removeButton);
+  
   
 });
-
 
 addElement();
